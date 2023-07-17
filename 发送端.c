b510f3cfe41e82258b9704c78a57ac663d1adac7
章大为 <62695176+zdwtest@users.noreply.github.com>
2023-07-17 15:43:37 +0800

    Update 发送端.c

--- 发送端.c
+++ 发送端.c
@@ -23,14 +23,14 @@ int main()
 	// 初始化Winsock库
 	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
 	{
-		error("Failed to initialize Winsock.");
+		error("无法初始化Winsock.");
 	}
 	
 	// 创建套接字
 	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 	if (sockfd == INVALID_SOCKET)
 	{
-		error("Failed to create socket.");
+		error("无法创建socket.");
 	}
 	
 	memset(&serverAddr, 0, sizeof(serverAddr));
@@ -38,13 +38,13 @@ int main()
 	
 	// 用户输入接收端的域名
 	char domainName[100];
-	printf("Enter the receiver's domain name: ");
+	printf("输入你要的发送的域名或者IP: ");
 	fgets(domainName, sizeof(domainName), stdin);
 	domainName[strcspn(domainName, "\n")] = '\0';
 	
 	// 用户输入接收端的端口号
 	unsigned short port;
-	printf("Enter the receiver's port number: ");
+	printf("输入接收者的端口: ");
 	scanf("%hu", &port);
 	getchar(); // 读取换行符
 	
@@ -69,7 +69,7 @@ int main()
 	while (1)
 	{
 		// 输入消息
-		printf("Enter a message: ");
+		printf("发送你的信息: ");
 		fgets(buffer, BUFFER_SIZE, stdin);
 		buffer[strcspn(buffer, "\n")] = '\0'; // 去除换行符
 		
@@ -77,20 +77,20 @@ int main()
 		int bytesSent = sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr *)&serverAddr, addrLen);
 		if (bytesSent == SOCKET_ERROR)
 		{
-			error("Failed to send message.");
+			error("发送失败.");
 		}
 		
 		// 接收回复消息
 		int bytesRead = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&serverAddr, &addrLen);
 		if (bytesRead == SOCKET_ERROR)
 		{
-			error("Failed to receive reply.");
+			error("无法收到回应.");
 		}
 		
 		buffer[bytesRead] = '\0'; // 添加字符串结束符
 		
 		// 打印回复消息
-		printf("Received reply: %s\n", buffer);
+		printf("回复: %s\n", buffer);
 	}
 	
 	// 关闭套接字
